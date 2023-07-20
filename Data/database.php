8b7a2ec197db13b357e12f464ad9c13dbcc78198
Quincy Tromp <quincy.tromp@hotmail.com>
2023-07-20 10:57:08 +0200

    Removed old tracked files from staging area

--- Data/database.php
+++ Data/database.php
@@ -1,14 +1,28 @@
 <?php 
 
-$servername = "localhost";
-$username = "webshop_user";
-$password = "AXN4OSdTm@ua]r4M";
-$dbname = "my_webshop";
-
-function connectToDB() {
-    $conn = new mysqli($servername, $username, $password, $dbname);
+function connectDB() {
+    define("SERVERNAME", "localhost");
+    define("USERNAME", "webshop_user");
+    define("PASSWORD", "AXN4OSdTm@ua]r4M");
+    define("DBNAME", "my_webshop");
+    $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
+    return $conn;
 }
 
+define("SERVERNAME", "localhost");
+define("USERNAME", "webshop_user");
+define("PASSWORD", "AXN4OSdTm@ua]r4M");
+define("DBNAME", "my_webshop");
+try {
+    $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+}
+catch(Exception $e) {
+    echo $e->getmessage();
+}
+echo "Connected succesfully to DB";
