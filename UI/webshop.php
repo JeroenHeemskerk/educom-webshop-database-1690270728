ef25e52ed38988490266d8b4dffbea8e089f75c0
Quincy Tromp <quincy.tromp@hotmail.com>
2023-07-25 11:58:48 +0200

    Arranged products into rows

--- UI/webshop.php
+++ UI/webshop.php
@@ -5,24 +5,37 @@ function showWebshopPage($data) {
     $products = getProducts();
     echo '  <div class="content">
                 <h1>Webshop</h1>
-                <div class="product_row">   
-                    ' . showProducts($products) . '
-                </div>
+                ' . showProducts($products) . '
             </div>';
 }
 
 
 function showProducts($products) {
     $content = "";
+    $counter = 0;
     foreach($products as $product => $value) {
-        $content .= '<div class="product_column">
-                        <img src="UI/Images/' . $products[$product]["filename"] . '" alt="picture of product">
-                        <div class="brand">' . $products[$product]["brand"] . '</div>
-                        <div class="product_name">' . $products[$product]["name"] . '</div>
-                        <div class="product_price">&euro;' . $products[$product]["price"] . '</div>
-                        ' . getAddToCart() . '
+        if ($counter % 2 == 0) {
+            $content .= '<div class="product_row">
+                            <div class="product_column">
+                                <img src="UI/Images/' . $products[$product]["filename"] . '" alt="picture of product">
+                                <div class="brand">' . $products[$product]["brand"] . '</div>
+                                <div class="product_name">' . $products[$product]["name"] . '</div>
+                                <div class="product_price">&euro;' . $products[$product]["price"] . '</div>
+                                ' . getAddToCart() . '
+                            </div>
+                        ';
+        }
+        else {
+            $content .= '<div class="product_column">
+                            <img src="UI/Images/' . $products[$product]["filename"] . '" alt="picture of product">
+                            <div class="brand">' . $products[$product]["brand"] . '</div>
+                            <div class="product_name">' . $products[$product]["name"] . '</div>
+                            <div class="product_price">&euro;' . $products[$product]["price"] . '</div>
+                            ' . getAddToCart() . '
+                        </div>
                     </div>';
-        
+        }
+        $counter += 1;
     }
     return $content;
 }
